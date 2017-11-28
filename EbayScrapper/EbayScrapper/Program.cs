b728aca78e6ef7b74025cd998eadbb238e53682b
RTG <igadget28@gmail.com>
2017-11-28 16:20:49 +0800

    Initial Commit

---
+++ EbayScrapper/EbayScrapper/Program.cs
@@ -0,0 +1,65 @@
+﻿using HtmlAgilityPack;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PropertyGuruScraper
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please enter your url: ");
+            var input = Console.ReadLine();
+            if (input == "")
+            {
+                Console.WriteLine("Please enter your url! Re-launch to add.");
+                Console.WriteLine("This is made by RTG, all rights reserved!");
+                Console.ReadLine();
+            }
+            else
+            {
+                GetHTMLAsync(input);
+                Console.ReadLine();
+            }
+        }
+
+        private static async void GetHTMLAsync(string link)
+        {
+            var url = link;
+            var httpClient = new HttpClient();
+            var html = await httpClient.GetStringAsync(url);
+
+            var htmlDocument = new HtmlDocument();
+            htmlDocument.LoadHtml(html);
+
+            var Products = htmlDocument.DocumentNode.Descendants("ul")
+                .Where(node => node.GetAttributeValue("id", "")
+                .Equals("ListViewInner")).ToList();
+
+            var ProductListItems = Products[0].Descendants("li")
+                .Where(node => node.GetAttributeValue("id", "")
+                .Contains("item")).ToList();
+
+            foreach (var ProductListItem in ProductListItems)
+            {
+                Console.WriteLine(ProductListItem.GetAttributeValue("listingid", ""));
+                Console.WriteLine(ProductListItem.Descendants("h3")
+                    .Where(node => node.GetAttributeValue("class", "")
+                    .Equals("lvtitle")).FirstOrDefault().InnerText.Trim('\r', '\t', '\n')
+                    );
+                Console.WriteLine(ProductListItem.Descendants("li")
+                    .Where(node => node.GetAttributeValue("class", "")
+                    .Equals("lvprice prc")).FirstOrDefault().InnerText.Trim('\r', '\t', '\n')
+                    );
+
+                Console.WriteLine();
+            }
+
+            Console.WriteLine("This is made by RTG, all rights reserved!");
+        }
+    }
+}
--- README.md
+++ README.md
@@ -1,1 +1,8 @@
 # EbayScrapper
+
+- You can add any link from *eBay*. If you want to edit, this code is pretty straightforward. Make sure you edit this using Visual Studio 2017 installed for .NET Frameworks for Microsoft Windows. 
+- I've also pre-built the .exe and placed inside the "EbayScrapperEXE" zip folder. If you're being suspicous, don't hesitate to scan for viruses.
+
+I tested this program using this link: https://www.ebay.com.my/sch/i.html?_nkw=xbox+one&_in_kw=1&_ex_kw=&_sacat=0&LH_Complete=1&_udlo=&_udhi=&_samilow=&_samihi=&_sadis=10&_fpos=&LH_SALE_CURRENCY=0&_sop=12&_dmd=1&_ipg=50
+
+#Thanks! :D
